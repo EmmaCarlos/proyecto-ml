fb05a9e46b72ab2bde804c71e515bcc5b14ec759
digitalhedu <evirgilio@digitalhouse.com>
2021-05-27 19:57:18 -0300

    final

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const app = express();
 
 app.set("port",process.env.PORT || 3000)
 
-app.listen(app.get("port"),() => console.log("Server Start"))
+app.listen(app.get("port"),() => console.log("Server Start http://localhost"+app.get("port")))
+app.use(express.static(path.resolve(__dirname,"./public")))
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"./views","index.html")))
